7c3fd845776617262e5ab97c0eb0eca393544da8
Thomas Pelowitz <tjp00004@gmail.com>
2023-02-24 10:31:44 -0600

    Update 344-Reverse_String.py

--- Easy/344 - Reverse String/344-Reverse_String.py
+++ Easy/344 - Reverse String/344-Reverse_String.py
@@ -4,7 +4,3 @@ class Solution:
         Do not return anything, modify s in-place instead.
         """
         s.reverse()
-        print(s)
-
-s = Solution()
-s.reverseString(["T", "h", "o", "m", "a", "s"])
