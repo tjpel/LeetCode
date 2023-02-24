fd958f53df53636eb981fd753faea82ee5caf8ad
Thomas Pelowitz <tjp00004@gmail.com>
2023-02-24 10:30:52 -0600

    24FEB23

---
+++ Easy/125 - Valid Palindrome/125-Valid_Palindrome.py
@@ -0,0 +1,18 @@
+class Solution:
+
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        cleaned = list(filter(lambda x: x.isalnum(), s))
+        print(cleaned)
+
+        for i in range(len(cleaned)):
+            if cleaned[i] != cleaned[-i-1]:
+                print(cleaned[i], i, cleaned[-i-1])
+                return False
+            
+        return True
+
+s = Solution()
+print(s.isPalindrome("A man, a plan, a canal: Paama"))
+
+        
