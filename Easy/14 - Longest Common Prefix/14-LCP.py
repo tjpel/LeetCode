d9526731f1a2f06ed585dee0683bdfbb06c8f2b2
tjpel <tjp00004@gmail.com>
2023-03-01 14:24:25 -0600

    Solved 14

---
+++ Easy/14 - Longest Common Prefix/14-LCP.py
@@ -0,0 +1,20 @@
+class Solution():
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        if len(strs) == 0:
+            return ""
+        elif len(strs) == 1:
+            return strs[0]
+        else:
+            
+            prefix = strs[0]
+
+            for s in strs[1:]:
+                while prefix != s[:len(prefix)]:
+                    prefix = prefix[:-1]
+                    
+            return prefix
+        
+s = Solution()
+print(s.longestCommonPrefix(["flower", "flow", "floght"]))
+
+        
